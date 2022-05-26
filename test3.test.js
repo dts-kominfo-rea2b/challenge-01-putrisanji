607b626d941346b6409305a70e4bd4003d1882f0
putri <putrisanjiwanidewi@gmail.com>
2022-05-26 13:04:37 +0900

    modify for handle length of data structure Set

--- test3.test.js
+++ test3.test.js
@@ -1,14 +1,13 @@
 const { users } = require("./index");
 
 describe("users - color & restaurant", () => {
-  it("should have unique favorite color attribute", async () => {    
-    expect(users[0].favoriteColor.length).toBe(4);
-    expect(users[1].favoriteColor.length).toBe(3);
-  });
-  
-  it("should have unique favorite restaurant attribute", async () => {    
-    expect(users[0].favoriteRestaurant.length).toBe(7);
-    expect(users[1].favoriteRestaurant.length).toBe(8);
-  });
-});
+    it("should have unique favorite color attribute", async () => {
+        expect(users[0].favoriteColor.size).toBe(4);
+        expect(users[1].favoriteColor.size).toBe(3);
+    });
 
+    it("should have unique favorite restaurant attribute", async () => {
+        expect(users[0].favoriteRestaurant.size).toBe(7);
+        expect(users[1].favoriteRestaurant.size).toBe(8);
+    });
+});
